70fa42ef6b16fee068b2abc3eb4efede01c309bb
ankitkushwaha4123 <ankitkushwahaak4123@gmail.com>
2025-09-01 23:22:15 +0530

    countnumb

--- countnumber.cpp
+++ countnumber.cpp
@@ -1,1 +1,27 @@
-anikitt f
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    int n;
+    cout<<"enter a number ";
+    cin>>n;
+    int count=0;
+    int lastdigit;
+
+    while(n>0)
+    {
+        lastdigit=n%10;
+        count=count+1;
+        cout<<lastdigit<<endl;
+
+
+        n=n/10;
+
+    }
+    cout <<"total digit "<< count<< endl;
+    return 0;
+
+}
+
