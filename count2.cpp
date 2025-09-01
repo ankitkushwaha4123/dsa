960b3253e4996467bd7daf961b4f987e6ac7d365
ankitkushwaha4123 <ankitkushwahaak4123@gmail.com>
2025-09-02 00:22:20 +0530

    second

---
+++ count2.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x = x / 10;
+
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7)) return 0;
+        
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8)) return 0;
+
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
+    }
+};
+
+int main() {
+    Solution sol;
+    int x;
+
+    cout << "Enter an integer: ";
+    cin >> x;
+
+    int result = sol.reverse(x);
+    cout << "Reversed integer: " << result << endl;
+
+    return 0;
+}
