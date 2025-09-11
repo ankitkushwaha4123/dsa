26ea043948dc58024754136d4a2e6d3caf1c7dd6
ankitkushwaha4123 <ankitkushwahaak4123@gmail.com>
2025-09-11 22:36:06 +0530

    duplicate

---
+++ dup2.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int removesuplicate(vector<int> &arr,int n)
+{
+    if(n==0) return 0;
+    int i=0;
+    for(int j=1;j<n;j++)
+    {
+        if(arr[i]!=arr[j])
+        {
+            arr[i+1]=arr[j];
+            i++;
+        }
+
+    }
+    return i+1;
+}
+int main()
+{
+    int n;
+    cout<<"ennter the number of element ";
+    cin>>n;
+
+    vector<int >arr(n);
+    cout<<"enter the element ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int newSize=removesuplicate(arr,n);
+
+    cout<<"array after removing duplicates:";
+    for(int i=0;i<newSize;i++)
+    {
+        cout<< arr[i]<<" ";
+
+    }
+    cout<<endl;
+    return 0;
+}
