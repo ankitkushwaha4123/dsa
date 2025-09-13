2e195f12612cedf82cd5fec23fd6694cf0e64049
ankitkushwaha4123 <ankitkushwahaak4123@gmail.com>
2025-09-13 22:55:12 +0530

    missing number arry

---
+++ missingnum.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int missing(vector<int> &a, int N)
+{
+    int xor1 = 0, xor2 = 0;
+    for (int i = 0; i < N - 1; i++)
+    {
+        xor2 = xor2 ^ a[i];   
+        xor1 = xor1 ^ (i + 1);  
+    }
+
+    xor1 = xor1 ^ N;
+    return xor1 ^ xor2;
+}
+
+int main()
+{
+    int N;
+    cout << "Enter the value of N (range 1..N): ";
+    cin >> N;
+
+    vector<int> a(N - 1);
+    cout << "Enter " << N - 1 << " numbers (1 to " << N << " with one missing): ";
+    for (int i = 0; i < N - 1; i++)
+    {
+        cin >> a[i];
+    }
+
+    int ans = missing(a, N);
+    cout << "The missing number is: " << ans << endl;
+
+    return 0;
+}
